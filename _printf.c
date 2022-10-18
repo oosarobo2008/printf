9f5a72ec1578cb995e1c5c8ab50601ea9ba3a3c5
ToniaEtim <abasimatim4@gmail.com>
2022-10-18 15:41:05 -0700

    printf project

--- _printf.c
+++ _printf.c
@@ -7,7 +7,6 @@
  *
  * Return: pointer to valid function or NULL
  */
-<<<<<<< HEAD
 static int (*check_for_specifiers(const char *format))(va_list)
 {
 	unsigned int i;
@@ -36,50 +35,9 @@ static int (*check_for_specifiers(const char *format))(va_list)
 		}
 	}
 	return (p[i].f);
-=======
-
-int _printf(const char *format, ...)
-{
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-return (-1);
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
-}
-else
-{
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
-}
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
->>>>>>> d23fecc8cc6801200565ece98c0652e5f3a07631
 }
 
 /**
-<<<<<<< HEAD
  * _printf - prints anything
  * @format: list of argument types passed to the function
  *
@@ -121,16 +79,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(valist);
 	return (count);
-=======
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-
-void print_buffer(char buffer[], int *buff_ind)
-{
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
->>>>>>> d23fecc8cc6801200565ece98c0652e5f3a07631
 }
