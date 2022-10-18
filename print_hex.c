0baadf3b0e96597a2ab052e633309e305a9170f8
oosarobo2008 <oosarobo2008@yahoo.com>
2022-10-18 14:45:46 -0700

    Team Task

--- print_hex.c
+++ print_hex.c
@@ -9,35 +9,34 @@
  */
 int print_hex(unsigned int n, unsigned int c)
 {
-  unsigned int a[8];
-  unsigned int i, m, sum;
-  char diff;
-  int count;
-
-  m = 268435456; /* (16 ^ 7) */
-  if (c)
-    diff = 'A' - ':';
-  else
-    diff = 'a' - ':';
-  a[0] = n / m;
-  for (i = 1; i < 8; i++)
-    {
-      m /= 16;
-      a[i] = (n / m) % 16;
-    }
-  for (i = 0, sum = 0, count = 0; i < 8; i++)
-    {
-      sum += a[i];
-      if (sum || i == 7)
-	{
-	  if (a[i] < 10)
-	    _putchar('0' + a[i]);
-	  else
-	    _putchar('0' + diff + a[i]);
-	  count++;
-	}
-    }
-  return (count);
+unsigned int a[8];
+unsigned int i, m, sum;
+char diff;
+int count;
+m = 268435456; /* (16 ^ 7) */
+if (c)
+diff = 'A' - ':';
+else
+diff = 'a' - ':';
+a[0] = n / m;
+for (i = 1; i < 8; i++)
+{
+m /= 16;
+a[i] = (n / m) % 16;
+}
+for (i = 0, sum = 0, count = 0; i < 8; i++)
+{
+sum += a[i];
+if (sum || i == 7)
+{
+if (a[i] < 10)
+_putchar('0' + a[i]);
+else
+_putchar('0' + diff + a[i]);
+count++;
+}
+}
+return (count);
 }
 /**
  * print_x - takes an unsigned int and prints it in lowercase hex notation
@@ -45,9 +44,10 @@ int print_hex(unsigned int n, unsigned int c)
  *
  * Return: number of digits printed
  */
+
 int print_x(va_list x)
 {
-  return (print_hex(va_arg(x, unsigned int), 0));
+return (print_hex(va_arg(x, unsigned int), 0));
 }
 
 /**
@@ -56,9 +56,10 @@ int print_x(va_list x)
  *
  * Return: number of digits printed
  */
+
 int print_X(va_list X)
 {
-  return (print_hex(va_arg(X, unsigned int), 1));
+return (print_hex(va_arg(X, unsigned int), 1));
 }
 
 /**
@@ -68,16 +69,16 @@ int print_X(va_list X)
  *
  * Return: base ^ exponent
  */
+
 static unsigned long _pow(unsigned int base, unsigned int exponent)
 {
-  unsigned int i;
-  unsigned long ans = base;
-
-  for (i = 1; i < exponent; i++)
-    {
-      ans *= base;
-    }
-  return (ans);
+unsigned int i;
+unsigned long ans = base;
+for (i = 1; i < exponent; i++)
+{
+ans *= base;
+}
+return (ans);
 }
 
 /**
@@ -86,45 +87,46 @@ static unsigned long _pow(unsigned int base, unsigned int exponent)
  *
  * Return: number of characters to print
  */
+
 int print_p(va_list p)
 {
-  int count = 0;
-  unsigned int a[16];
-  unsigned int i, sum;
-  unsigned long n, m;
-  char *str = "(nil)";
+int count = 0;
+unsigned int a[16];
+unsigned int i, sum;
+unsigned long n, m;
+char *str = "(nil)";
 
-  n = va_arg(p, unsigned long);
-  if (n == 0)
-    {
-      for (i = 0; str[i]; i++)
-	{
-	  _putchar(str[i]);
-	  count++;
-	}
-      return (count);
-    }
-  _putchar('0');
-  _putchar('x');
-  count = 2;
-  m = _pow(16, 15); /* 16 ^ 15 */
-  a[0] = n / m;
-  for (i = 1; i < 16; i++)
-    {
-      m /= 16;
-      a[i] = (n / m) % 16;
-    }
-  for (i = 0, sum = 0; i < 16; i++)
-    {
-      sum += a[i];
-      if (sum || i == 15)
-	{
-	  if (a[i] < 10)
-	    _putchar('0' + a[i]);
-	  else
-	    _putchar('0' + ('a' - ':') + a[i]);
-	  count++;
-	}
-    }
-  return (count);
+n = va_arg(p, unsigned long);
+if (n == 0)
+{
+for (i = 0; str[i]; i++)
+{
+_putchar(str[i]);
+count++;
+}
+return (count);
+}
+_putchar('0');
+_putchar('x');
+count = 2;
+m = _pow(16, 15); /* 16 ^ 15 */
+a[0] = n / m;
+for (i = 1; i < 16; i++)
+{
+m /= 16;
+a[i] = (n / m) % 16;
+}
+for (i = 0, sum = 0; i < 16; i++)
+{
+sum += a[i];
+if (sum || i == 15)
+{
+if (a[i] < 10)
+_putchar('0' + a[i]);
+else
+_putchar('0' + ('a' - ':') + a[i]);
+count++;
+}
+}
+return (count);
 }
